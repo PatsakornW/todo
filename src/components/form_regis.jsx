d9596211bc75e6f33ef27a4e0d31f72f913328c9
PatsakornW <patsakorn.wtnsb@gmail.com>
2023-11-22 01:42:56 +0700

    Update form_regis.jsx

--- src/components/form_regis.jsx
+++ src/components/form_regis.jsx
@@ -10,12 +10,12 @@ const Form_regis = ({ setToggle }) => {
     } else if (
       !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
     ) {
-      errors.email = "Invalid email address";
+      errors.email = "Invalid email";
     }
 
     if (!values.password) {
       errors.password = "Required";
-    } else if (values.password.length <= 8) {
+    } else if (values.password.length < 8) {
       errors.password = "Must be at least 8 characters";
     }
     return errors;
@@ -45,7 +45,7 @@ const Form_regis = ({ setToggle }) => {
               id="email"
               name="email"
               type="email"
-              label="Email Address"
+              label="Email"
               variant="filled"
               onChange={formik.handleChange}
               value={formik.values.email}
